dbf690c7a764257b2b91e4699994626d50244779
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-10 18:46:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min = 289
+  retkey = ""
+  name_hash.each do |key, value|
+    if (key < min)
+      min = key
+      retkey = key
+    end
+  end
+  retkey
 end
